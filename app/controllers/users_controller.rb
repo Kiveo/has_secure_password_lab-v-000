d8b13f216e597b6fa89ac4fec64d45ea5294c295
Maverick Tesini <mavericktesini@gmail.com>
2018-08-05 01:23:36 +0000

    automatically backed up by learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,12 @@
 class UsersController < ApplicationController
 
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user.create(username: params[:username], password: params[:password])
+    redirect '/' #welcome page
+  end
 
 end 
