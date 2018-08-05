ca066f2d17a5f7e6418c34db42976adb9572ca2c
Maverick Tesini <mavericktesini@gmail.com>
2018-08-05 01:58:08 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,8 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'welcome#index'
 
-  resources :users, only: [:new, :create]
+  resources :users
+
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
 
