0c74f005bb8c492743407e6bb0dfbc792943c2df
Maverick Tesini <mavericktesini@gmail.com>
2018-08-05 02:09:46 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
    end
  end
 
-  Log In action
+  # Log In action
   def create
     @user = User.find_by(name: params[:name])
     if @user && @user.authenticate(params[:password])
