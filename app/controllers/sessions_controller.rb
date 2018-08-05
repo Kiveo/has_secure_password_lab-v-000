285e55a83243aafb5ba845e8cd6625c3c17985f5
Maverick Tesini <mavericktesini@gmail.com>
2018-08-05 02:20:56 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,25 +9,15 @@ class SessionsController < ApplicationController
  end
 
   # Log In action
-  # def create
-  #   @user = User.find_by(name: params[:name])
-  #   if @user && @user.authenticate(params[:password])
-  #     session[:user_id] = @user.id
-  #     redirect_to "/" #home page
-  #   else
-  #     flash[:notice] = "Incorrect Login Info"
-  #     redirect_to new_user_path
-  #   end
-  #  end
-
-   def create
-     @user = User.find_by(name: params[:name])
-     if @user
-       return head(:forbidden) unless @user.authenticate(params[:password])
-       session[:user_id] = @user.id
-     else
-       redirect_to new_session_path
-     end
+  def create
+    @user = User.find_by(name: params[:name])
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect_to "/" #home page
+    else
+      flash[:notice] = "Incorrect Login Info"
+      redirect_to new_user_path
+    end
    end
 
   def delete
