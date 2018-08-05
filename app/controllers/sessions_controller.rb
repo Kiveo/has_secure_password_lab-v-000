09db0e8bfc8c1f99394c39a600698e9c8ab12edd
Maverick Tesini <mavericktesini@gmail.com>
2018-08-05 02:25:08 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,25 +9,25 @@ class SessionsController < ApplicationController
  end
 
   # Log In action
-  # def create
-  #   @user = User.find_by(name: params[:name])
-  #   if @user && @user.authenticate(params[:password])
-  #     session[:user_id] = @user.id
-  #     redirect_to "/" #home page
-  #   else
-  #     flash[:notice] = "Incorrect Login Info"
-  #     redirect_to new_user_path
-  #   end
-  #  end
+  def create
+    @user = User.find_by(name: params[:name])
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect_to "/" #home page
+    else
+      flash[:notice] = "Incorrect Login Info"
+      redirect_to '/'
+    end
+   end
 # ---------------------------
-def create
-    user = User.find_by(name: params[:user][:name])
-    user = user.try(:authenticate, params[:user][:password])
-    return redirect_to(controller: 'sessions', action: 'new') unless user
-    @user = user
-    session[:user_id] = @user.id
-    redirect_to '/'
-end
+# def create
+#     user = User.find_by(name: params[:user][:name])
+#     user = user.try(:authenticate, params[:user][:password])
+#     return redirect_to(controller: 'sessions', action: 'new') unless user
+#     @user = user
+#     session[:user_id] = @user.id
+#     redirect_to '/'
+# end
 # -----------------------
   def delete
     session.delete :user_id
