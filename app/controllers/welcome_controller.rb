338e5637d66902b3b0e00e576307ab28373d81f4
Maverick Tesini <mavericktesini@gmail.com>
2018-08-05 01:56:01 +0000

    automatically backed up by learn

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,6 +2,11 @@ class WelcomeController < ApplicationController
 
   def index
     @user = User.find_by(id: session[:user_id])
+    if @user
+      @greeting_name = @user.name
+    else
+      @greeting_name = 'Guest'
+    end
   end
 
 end
