5b63ff0282ebb7a505fe616773d45bbdf804ca97
Maverick Tesini <mavericktesini@gmail.com>
2018-08-05 02:05:13 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -11,17 +11,17 @@ class SessionsController < ApplicationController
   # Log In action
   def create
     @user = User.find_by(name: params[:name])
-    if @user && @user.authenticate(params[:password])
+    if @user.authenticate(params[:password])
       session[:user_id] = @user.id
       redirect_to "/" #home page
     else
       flash[:notice] = "Incorrect Login Info"
       render 'sessions/new'
     end
-   end
+  end
 
- def delete
-   session.delete :user_id
- end
+  def delete
+    session.delete :user_id
+  end
 
 end
