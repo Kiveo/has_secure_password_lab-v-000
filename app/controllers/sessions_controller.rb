61fedb3b891c3743782a433c9744f7f000c1821f
Maverick Tesini <mavericktesini@gmail.com>
2018-08-05 02:00:53 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,7 +10,7 @@ class SessionsController < ApplicationController
 
   # Log In action
   def create
-    @user = User.find_by(username: params[:username])
+    @user = User.find_by(name: params[:name])
     if @user && @user.authenticate(params[:password])
       session[:user_id] = @user.id
       redirect_to "/" #home page
